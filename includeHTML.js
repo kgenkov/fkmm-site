9109ef436543dc6e04063d9443aa3714aabc8add
Kaloyan Genkov <kgenkov@uni-sofia.bg>
2020-02-18 18:06:44 +0200

    Implement dynamic loading of content; Add contact.html

--- includeHTML.js
+++ includeHTML.js
@@ -25,7 +25,28 @@ function includeHTML() {
     }
   }
 }
-function load(path="index.html") {
-     document.getElementById("content").innerHTML='<object type="text/html" data="menu.html"></object>';
+function load(path="home") {
+      /* Make an HTTP request using the attribute value as the file name: */
+      elmnt = document.getElementById("content");
+      document.getElementById("title").innerHTML='';
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "<h2 class='w3-panel w3-card w3-border w3-leftbar w3-border-red w3-pale-yellow w3-center w3-padding-16'>Страницата не е намерена!</h2>";}
+        }
+      }
+      xhttp.open("GET", path, true);
+      xhttp.send();      
+      /* Exit the function: */
+      return;
 }
 
+function loadMainContent() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams (queryString);
+    var page = urlParams.get ('page');
+    if (page == null) page = "home";
+    console.log (page);
+    load(page+".html");
+}
